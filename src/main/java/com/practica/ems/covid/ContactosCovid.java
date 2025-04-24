f2b56c110f8a3214ce553b64223eae11f74a3ad6
Pablo Macías <pablomaciasbrad@gmail.com>
2025-04-24 10:15:49 +0200

    switch excesivo en crearPersona arreglado

--- src/main/java/com/practica/ems/covid/ContactosCovid.java
+++ src/main/java/com/practica/ems/covid/ContactosCovid.java
@@ -201,32 +201,14 @@ public class ContactosCovid {
 
 	private Persona crearPersona(String[] data) {
 		Persona persona = new Persona();
-		for (int i = 1; i < Constantes.MAX_DATOS_PERSONA; i++) {
-			String s = data[i];
-			switch (i) {
-			case 1:
-				persona.setDocumento(s);
-				break;
-			case 2:
-				persona.setNombre(s);
-				break;
-			case 3:
-				persona.setApellidos(s);
-				break;
-			case 4:
-				persona.setEmail(s);
-				break;
-			case 5:
-				persona.setDireccion(s);
-				break;
-			case 6:
-				persona.setCp(s);
-				break;
-			case 7:
-				persona.setFechaNacimiento(parsearFecha(s));
-				break;
-			}
-		}
+		int i = 1;
+		persona.setDocumento(data[i++]);
+		persona.setNombre(data[i++]);
+		persona.setApellidos(data[i++]);
+		persona.setEmail(data[i++]);
+		persona.setDireccion(data[i++]);
+		persona.setCp(data[i++]);
+		persona.setFechaNacimiento(parsearFecha(data[i++]));
 		return persona;
 	}
 
